b0c7a15fb876520c7ada1ef71a3dbefdd6fd7ed9
andrewandrew <andrew@zakonov.com>
2015-07-14 01:36:14 +0300

    user Event function

--- jdevmetrics.js
+++ jdevmetrics.js
@@ -19,3 +19,25 @@ function devmetricsEvent(event, eventOptions, libOptions) {
     dataType: 'json'
   });
 }
+
+/**
+ * @param event_name Metric name, all events with same metric name are aggregated
+ * @param user_id optional, User unique identifier, used only for log
+ * @param event_tags optional, Additional info, metric dimensions, string or array
+ */
+function devmetricsUserEvent(event_name, user_id, event_tags) {
+    $.ajax({
+        method: 'POST',
+        url: '/api/js_userevents',
+        data: {
+            'event_name': event_name,
+            'user_id': user_id,
+            'event_tags': event_tags
+        },
+        success: function(res) {
+        },
+        error: function(res) {
+        },
+        dataType: 'json'
+    });
+}
