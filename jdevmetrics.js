6985693ba3eb35be5eef68060bcdcb763ee3305d
andrewandrew <andrew@zakonov.com>
2015-09-07 00:56:20 +0300

    js lib support web

--- jdevmetrics.js
+++ jdevmetrics.js
@@ -3,6 +3,7 @@
   var devmetrics = {
     init: function (app_id) {
       window.dm_app_id = app_id;
+      this.sendData('lib_init');
     },
     sendData: function (event_name, user_id, event_tags) {
       var req = new XMLHttpRequest();
@@ -14,10 +15,7 @@
       req.onreadystatechange = function (e) {
         if (req.readyState === 4) {
           if (req.status === 200) {
-            console.log('ok');
           } else {
-            var error = 'Bad HTTP status: ' + req.status + ' ' + req.statusText;
-            console.error(error);
           }
         }
       };
@@ -31,50 +29,6 @@
   };
 
   window.devmetrics = devmetrics;
-
-  /**
-   * Send event to localhost api. jQuery is required.
-   * @param event string, event name
-   * @param eventOptions array, addition event properties
-   * @param libOptions array, lib options, like host and token
-   */
-  function devmetricsEvent(event, eventOptions, libOptions) {
-    $.ajax({
-      method: 'POST',
-      url: '/api/jsevents',
-      data: {
-        'name': event,
-        'options': eventOptions
-      },
-      success: function (res) {
-      },
-      error: function (res) {
-      },
-      dataType: 'json'
-    });
-  }
-
-  /**
-   * @param event_name Metric name, all events with same metric name are aggregated
-   * @param user_id optional, User unique identifier, used only for log
-   * @param event_tags optional, Additional info, metric dimensions, string or array
-   */
-  function devmetricsUserEvent(event_name, user_id, event_tags) {
-    $.ajax({
-      method: 'POST',
-      url: '/api/js_userevents',
-      data: {
-        'event_name': event_name,
-        'user_id': user_id,
-        'event_tags': event_tags || []
-      },
-      success: function (res) {
-      },
-      error: function (res) {
-      },
-      dataType: 'json'
-    });
-  }
 
 
 //});
