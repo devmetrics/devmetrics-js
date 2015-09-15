d533d653baf01332b53b573ccb712b3d3b6174fb
andrewandrew <andrew@zakonov.com>
2015-09-15 22:38:38 +0300

    fully async version of lib, referrer support, improved cookie, better api

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # devmetrics-js
+
+The devmetrics.io JS library for client side instrumentation.
+
+Devmetrics – Opensource Usage Analytic for Web.
+
+Visit www.devmetrics.io to generate an app_id.
+
+# Installation
+
+Add the following code into the <head> section of your HTML page to start tracking user activity.
+
+```html
+<script>
+      (function (doc, inst) {
+          window.devmetrics = inst; inst.q = inst.q || []; inst.init = function (token) { this.token = token; }; inst.userEvent = function (eventName) { inst.q.push(eventName); };
+          var a = doc.createElement("script"); a.type = "text/javascript"; a.src = '/js/tmp_js.js?' + Math.floor(((new Date()).getTime() - 1442107445573) / 8640000); var e = doc.getElementsByTagName("script")[0]; e.parentNode.insertBefore(a, e);
+      })(document, window.devmetrics || []);
+      devmetrics.init("your-app-id");
+      devmetrics.userEvent('page_' + location.pathname.substr(1));
+</script>
+```
+
+
+# Tracking an event
+
+Tracking an event is as simple as one line of code.
+
+```html
+<script>
+      devmetrics.userEvent('call2act_button_click');
+</script>
+```
+
+# Dashboards to view events and for analytic
+
+www.devmetrics.com/account
--- jdevmetrics.js
+++ jdevmetrics.js
@@ -1,34 +1,119 @@
-//(function () {
+(function (devmetrics) {
+  'use strict';
 
-  var devmetrics = {
-    init: function (app_id) {
-      window.dm_app_id = app_id;
-      this.sendData('lib_init');
-    },
-    sendData: function (event_name, user_id, event_tags) {
-      var req = new XMLHttpRequest();
-      var url = 'http://www.devmetrics.io/api/event';
-      url += '?app_id=' + window.dm_app_id;
-      url += '&event_name=' + event_name;
-      url += '&user_id=' + user_id;
-      req.open("GET", url, true);
-      req.onreadystatechange = function (e) {
-        if (req.readyState === 4) {
-          if (req.status === 200) {
-          } else {
-          }
-        }
-      };
-      req.send(null);
-    },
-    userEvent: function (event_name, user_id, event_tags) {
-      event_name = event_name.replace(/[\W_]+/g, "_");
-      user_id = (user_id || '-1');
-      devmetrics.sendData(event_name, user_id, event_tags);
+  var devmetrics = window.devmetrics || [];
+  window.devmetricsdata = {};
+
+  devmetrics._genUid = function () {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
     }
+
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
   };
 
-  window.devmetrics = devmetrics;
+  devmetrics._saveReferrer = function () {
+    window.devmetricsdata['referrer'] = document.referrer;
+  };
 
+  devmetrics._savePage = function () {
+    window.devmetricsdata['page'] = document.location.href;
+  };
 
-//});
+  devmetrics._registerUser = function (explicit_id) {
+    var uid = explicit_id || window.dm_uid;
+    if (!uid) {
+      uid = this.getData('dm_uid');
+    }
+    if (!uid) {
+      uid = this._genUid();
+      this.setData('dm_uid', uid);
+    }
+    window.dm_uid = uid;
+  };
+
+  devmetrics.setData = function (name, value) {
+    var d = new Date();
+    d.setTime(d.getTime() + (60 * 24 * 60 * 60 * 1000)); // 60 days
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + "; " + expires;
+  };
+
+  devmetrics.getData = function (name) {
+    var name = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1);
+      if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+  };
+
+  /**
+   * Set explicit user id
+   * @param user_id
+   */
+  devmetrics.setUserId = function (user_id) {
+    this._registerUser(user_id);
+  }
+
+  /**
+   * Init lib
+   * @param app_id
+   */
+  devmetrics.init = function (app_id) {
+    window.dm_app_id = app_id;
+    this._saveReferrer();
+    this._savePage();
+    this._registerUser();
+    this.sendData('lib_init');
+  };
+
+  devmetrics.sendData = function (event_name, event_tags) {
+    var req = new XMLHttpRequest();
+    var url = 'http://www.devmetrics.io/api/event';
+    url += '?app_id=' + window.dm_app_id;
+    url += '&event_name=' + event_name;
+    url += '&user_id=' + window.dm_uid;
+    if (window.devmetricsdata['referrer']) {
+      url += '&referrer=' + window.devmetricsdata['referrer'];
+    }
+    if (window.devmetricsdata['page']) {
+      url += '&page=' + window.devmetricsdata['page'];
+    }
+    req.open("GET", url, true);
+    req.onreadystatechange = function (e) {
+      if (req.readyState === 4) {
+        if (req.status === 200) {
+        } else {
+        }
+      }
+    };
+    req.send(null);
+  };
+
+  /**
+   * Send user event data
+   * @param event_name
+   * @param event_tags
+   */
+  devmetrics.userEvent = function (event_name, event_tags) {
+    event_name = event_name.replace(/[\W_]+/g, "_");
+    devmetrics.sendData(event_name, event_tags);
+  };
+
+  //// Postprocess early init data
+  if (devmetrics.token) { //dyn load set
+    devmetrics.init(devmetrics.token);
+  }
+  if (devmetrics.q) { //dyn load queue
+    for (var i = 0; i < devmetrics.q.length; ++i) {
+      devmetrics.userEvent(devmetrics.q[i]);
+    }
+  }
+
+})(window['devmetrics']);
